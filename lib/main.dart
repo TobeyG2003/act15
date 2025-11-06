ac3306cdd031bf5c37140451a7a79718028ca636
Tobey <TobeyG2003@gmail.com>
2025-11-05 21:02:49 -0500

    adds service and item classes

--- lib/main.dart
+++ lib/main.dart
@@ -50,3 +50,72 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class Item {
+  String? id;
+  final String name;
+  final int quantity;
+  final double price;
+  final String category;
+  final DateTime createdAt;
+
+  Item({
+    this.id,
+    required this.name,
+    required this.quantity,
+    required this.price,
+    required this.category,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'quantity': quantity,
+      'price': price,
+      'category': category,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      quantity: map['quantity'],
+      price: map['price'],
+      category: map['category'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+
+}
+
+class FirestoreService {
+  final CollectionReference itemsCollection =
+      FirebaseFirestore.instance.collection('items');
+
+  Future<void> addItem(Item item) async {
+    DocumentReference docRef = await itemsCollection.add(item.toMap());
+    item.id = docRef.id;
+    await docRef.update({'id': item.id});
+  }
+
+  Stream<Item?> getItemsStream(String id) {
+    return itemsCollection.doc(id).snapshots().map((docSnap) {
+      if (docSnap.exists) {
+        return Item.fromMap(docSnap.data() as Map<String, dynamic>);
+      }
+      return null;
+    });
+  }
+
+  Future<void> updateItem(Item item) async {
+    await itemsCollection.doc(item.id).update(item.toMap());
+  }
+
+  Future<void> deleteItem(String id) async {
+    await itemsCollection.doc(id).delete();
+  }
+}
