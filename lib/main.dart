f4d552bf1645fc002a38414a8b018af54771c456
Tobey <TobeyG2003@gmail.com>
2025-11-06 19:29:37 -0500

    adds update edit functions

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _searchController = TextEditingController();
 
   final CollectionReference itemsCollection =
       FirebaseFirestore.instance.collection('items');
@@ -46,12 +47,27 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: StreamBuilder<List<Item>>(
-        stream: FirestoreService().getItemsStream(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Column(
+      body: Column(
+        children: [
+          TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              labelText: 'Search',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () {
+                  _searchController.clear();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<List<Item>>(
+              stream: FirestoreService().getItemsStream(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const Icon(Icons.error, color: Colors.red, size: 60),
@@ -82,27 +98,50 @@ class _MyHomePageState extends State<MyHomePage> {
                 return ListTile(
                   title: Text(item.name),
                   subtitle: Text('Quantity: ${item.quantity}, Price: \$${item.price}, Category: ${item.category}, Created At: ${item.createdAt}'),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => AddEditItemScreen(item: item),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             );
           }
-          
           return const Center(
             child: Text('No data available'),
           );
         }
-      )
+            ),
+          ),
+        ],  
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => AddEditItemScreen(),
+            ),
+          );
+        },
+        tooltip: 'Add Item',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
 
 class Item {
   String? id;
-  final String name;
-  final int quantity;
-  final double price;
-  final String category;
-  final DateTime createdAt;
+  String name;
+  int quantity;
+  double price;
+  String category;
+  DateTime createdAt;
 
   Item({
     this.id,
@@ -166,5 +205,120 @@ class FirestoreService {
 
   Future<void> deleteItem(String id) async {
     await itemsCollection.doc(id).delete();
+  }
+}
+
+class AddEditItemScreen extends StatefulWidget {
+  final Item? item;
+  AddEditItemScreen({Key? key, this.item}) : super(key: key);
+  @override
+  _AddEditItemScreenState createState() => _AddEditItemScreenState();
+}
+
+class _AddEditItemScreenState extends State<AddEditItemScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController categoryController = TextEditingController();
+
+  bool isEditMode = false;
+  
+  @override
+  void initState() {
+    super.initState();
+    if (widget.item == null) {
+      isEditMode = false;
+    } else {
+      isEditMode = true;
+      // Set controller values for edit mode
+      nameController.text = widget.item!.name;
+      quantityController.text = widget.item!.quantity.toString();
+      priceController.text = widget.item!.price.toString();
+      categoryController.text = widget.item!.category;
+    }
+  }
+  
+  @override
+  void dispose() {
+    // Clean up controllers
+    nameController.dispose();
+    quantityController.dispose();
+    priceController.dispose();
+    categoryController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEditMode ? 'Edit Item' : 'Add Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: quantityController,
+                decoration: const InputDecoration(labelText: 'Quantity'),
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: priceController,
+                decoration: const InputDecoration(labelText: 'Price'),
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              ),
+              TextFormField(
+                controller: categoryController,
+                decoration: const InputDecoration(labelText: 'Category'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (isEditMode) {
+                    widget.item!.name = nameController.text;
+                    widget.item!.quantity = int.parse(quantityController.text);
+                    widget.item!.price = double.parse(priceController.text);
+                    widget.item!.category = categoryController.text;
+                    FirestoreService().updateItem(widget.item!);
+                  } else {
+                    final newItem = Item(
+                      name: nameController.text,
+                      quantity: int.parse(quantityController.text),
+                      price: double.parse(priceController.text),
+                      category: categoryController.text,
+                      createdAt: DateTime.now(),
+                    );
+                    FirestoreService().addItem(newItem);
+                  }
+                  Navigator.pop(context);
+                },
+                child: Text(isEditMode ? 'Update' : 'Add'),
+              ),
+              if (isEditMode)
+                ElevatedButton(
+                  onPressed: () {
+                    FirestoreService().deleteItem(widget.item!.id!);
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text('Delete'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
